c779d74fd9785bd1dcf4ebd0ea11801a06ad243d
notstevy <notstevy@ultrabuildmc.de>
2025-01-01 14:48:10 +0100

    Update command api

--- kotlin-paper/build.gradle.kts
+++ kotlin-paper/build.gradle.kts
@@ -12,8 +12,8 @@ paperPluginYaml {
 dependencies {
     paperweight.devBundle("world.kitpvp.kitpvpslime", "1.21.1-R0.1-SNAPSHOT")
 
-    api("dev.jorel:commandapi-bukkit-shade-mojang-mapped:9.5.3") // https://github.com/JorelAli/CommandAPI/releases/latest
-    api("dev.jorel:commandapi-bukkit-kotlin:9.5.3")
+    api("dev.jorel:commandapi-bukkit-shade-mojang-mapped:9.7.0") // https://github.com/JorelAli/CommandAPI/releases/latest
+    api("dev.jorel:commandapi-bukkit-kotlin:9.7.0")
 
     api(libs.kotlinx.coroutines)
     api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk9:${libs.versions.kotlinx.coroutines.get()}")
