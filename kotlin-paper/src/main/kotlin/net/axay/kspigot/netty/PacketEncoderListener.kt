e7d39bc73184d667e64d85382a3a849b716556c4
notstevy <notstevy@ultrabuildmc.de>
2025-05-09 17:51:15 +0200

    Fix packet events incompatibility

--- kotlin-paper/src/main/kotlin/net/axay/kspigot/netty/PacketEncoderListener.kt
+++ kotlin-paper/src/main/kotlin/net/axay/kspigot/netty/PacketEncoderListener.kt
@@ -1,39 +1,33 @@
 package net.axay.kspigot.netty
 
 import io.netty.channel.ChannelHandlerContext
-import io.netty.handler.codec.MessageToMessageEncoder
+import io.netty.channel.ChannelOutboundHandlerAdapter
+import io.netty.channel.ChannelPromise
 import net.minecraft.network.protocol.Packet
 import org.bukkit.entity.Player
-import kotlin.collections.plusAssign
 
-class PacketEncoderListener(private val player: Player): MessageToMessageEncoder<Any>() {
-    override fun encode(
-        ctx: ChannelHandlerContext,
-        msg: Any,
-        out: MutableList<Any>
-    ) {
-        if(msg !is Packet<*>) {
-            out += msg
+class PacketEncoderListener(private val player: Player) : ChannelOutboundHandlerAdapter() {
+    override fun write(ctx: ChannelHandlerContext, msg: Any, promise: ChannelPromise) {
+        if (msg !is Packet<*>) {
+            super.write(ctx, msg, promise)
             return
         }
 
         if (msg is ProtectedPacket) {
             val packet = msg.originalPacket
-            out += packet
+            super.write(ctx, packet, promise)
             return
         }
-
         val clazz = msg.javaClass
         val listeners = PacketHandler.customPacketListeners[clazz]
-        if(listeners == null)  {
-            out += msg
+        if (listeners == null) {
+            super.write(ctx, msg, promise)
             return
         }
-
         val event = PacketEvent(msg, player, ctx)
         listeners.forEach { it(event) }
         if (event.cancelled)
             return
-        out += msg
+        super.write(ctx, msg, promise)
     }
 }
